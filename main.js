59cad816cdf8bd35ebbda0604c772f2317c4ac0e
arc-alex <ac@omnivox.dk>
2023-09-01 14:16:49 +0200

    init

--- main.js
+++ main.js
@@ -3,23 +3,53 @@ const UpgradeScripts = require('./upgrades')
 const UpdateActions = require('./actions')
 const UpdateFeedbacks = require('./feedbacks')
 const UpdateVariableDefinitions = require('./variables')
+import('node-fetch')
 
 class ModuleInstance extends InstanceBase {
 	constructor(internal) {
 		super(internal)
 	}
 
+	async getSatellite() {
+		try {
+			const response = await fetch(this.url)
+			const json = await response.json()
+			this.updateStatus(InstanceStatus.Ok)
+			if (json.host) {
+				this.setVariableValues({ host: json.host })
+			}
+			if (json.port) {
+				this.setVariableValues({ port: json.port })
+			}
+			if (json.connected) {
+				this.setVariableValues({ con: json.connected })
+			}
+		} catch (error) {
+			console.error(error)
+			this.updateStatus(InstanceStatus.Disconnected)
+		}
+	}
+
 	async init(config) {
 		this.config = config
 
-		this.updateStatus(InstanceStatus.Ok)
+		this.url = 'http://' + this.config.host + ':' + String(this.config.port) + '/api/config'
+
+		this.updateStatus(InstanceStatus.Connecting)
 
 		this.updateActions() // export actions
 		this.updateFeedbacks() // export feedbacks
 		this.updateVariableDefinitions() // export variable definitions
+
+		this.getSatellite()
+
+		this.interval = setInterval(() => {
+			this.getSatellite()
+		}, 10000)
 	}
 	// When module gets deleted
 	async destroy() {
+		clearInterval(this.interval)
 		this.log('debug', 'destroy')
 	}
 
@@ -33,14 +63,14 @@ class ModuleInstance extends InstanceBase {
 			{
 				type: 'textinput',
 				id: 'host',
-				label: 'Target IP',
+				label: 'Satellite IP',
 				width: 8,
 				regex: Regex.IP,
 			},
 			{
 				type: 'textinput',
 				id: 'port',
-				label: 'Target Port',
+				label: 'REST Port',
 				width: 4,
 				regex: Regex.PORT,
 			},
