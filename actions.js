016bf0fae322ef5715c6679cbec6c5ba042bcf5b
arc-alex <ac@omnivox.dk>
2023-09-01 15:04:59 +0200

    set host

--- actions.js
+++ actions.js
@@ -1,19 +1,38 @@
+import('node-fetch')
+
 module.exports = function (self) {
 	self.setActionDefinitions({
-		sample_action: {
-			name: 'My First Action',
+		set_host: {
+			name: 'Set Host',
 			options: [
 				{
-					id: 'num',
+					id: 'host',
+					type: 'textinput',
+					label: 'Host IP',
+					default: '127.0.0.1',
+				},
+				{
+					id: 'port',
 					type: 'number',
-					label: 'Test',
-					default: 5,
-					min: 0,
-					max: 100,
+					label: 'Satellite Port',
+					default: 16622,
 				},
 			],
 			callback: async (event) => {
-				console.log('Hello world!', event.options.num)
+				// console.log('debug', 'set host ' + event.options.host)
+				self.log('debug', 'set host ' + JSON.stringify({ host: event.options.host, port: event.options.port }))
+				try {
+					await fetch(self.url, {
+						// signal: AbortSignal.timeout(self.config.timeout),
+						method: 'POST',
+						headers: {
+							'Content-Type': 'application/json',
+						},
+						body: JSON.stringify({ host: event.options.host, port: event.options.port }),
+					})
+				} catch (error) {
+					self.log('error', 'post' + JSON.stringify({ host: event.options.host, port: event.options.port }))
+				}
 			},
 		},
 	})
